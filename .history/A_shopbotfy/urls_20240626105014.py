3e586597acaf69f2e46e2dd38b8535c2dce6aad7
ronildob <barrosronildo@gmail.com>
2024-06-26 10:51:29 -0300

    delete home

--- .history/A_shopbotfy/urls_20240626105014.py
+++ .history/A_shopbotfy/urls_20240626105014.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import home_page, about_page, contact_page, login_page, register_page
 from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view
 
